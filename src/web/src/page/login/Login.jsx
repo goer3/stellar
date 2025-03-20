96b849fd1bd6be3f0271451816c9785f34a98106
DK <1214966109@qq.com>
2025-03-21 01:20:34 +0800

    登录页面

--- src/web/src/page/login/Login.jsx
+++ src/web/src/page/login/Login.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
-import { Layout } from 'antd';
-import { GithubOutlined } from '@ant-design/icons';
+import { Layout, Divider, Form, Input, Button, Checkbox } from 'antd';
+import { GithubOutlined, UserOutlined, LockOutlined, InsuranceOutlined, DingtalkOutlined } from '@ant-design/icons';
 import { Logo } from '@/components/Image';
 
 const { Header, Content } = Layout;
@@ -25,6 +25,11 @@ const Login = () => {
     }
   };
 
+  // 登录处理
+  const loginHandler = (values) => {
+    console.log(values);
+  };
+
   return (
     <Layout className="stellar-login">
       <Header className="stellar-login-header">
@@ -38,6 +43,76 @@ const Login = () => {
         </div>
       </Header>
       <Content className="stellar-login-content">
+        <div className="stellar-login-box">
+          <div className="stellar-login-title">登录 / Sign in</div>
+          <Divider className="stellar-login-welcome">欢迎回来</Divider>
+          <div className="stellar-login-form">
+            <Form
+              name="login"
+              onFinish={loginHandler}>
+              <Form.Item
+                name="account"
+                rules={[
+                  {
+                    min: 4,
+                    max: 30,
+                    message: '账户长度范围为 4-30 个字符'
+                  },
+                  {
+                    required: true,
+                    message: '请输入您的工号 / 手机号 / Email'
+                  }
+                ]}>
+                <Input autoComplete="off" prefix={<UserOutlined />} placeholder="工号 / 手机号 / Email" />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    min: 8,
+                    max: 30,
+                    message: '密码长度范围为 8-30 个字符'
+                  },
+                  {
+                    required: true,
+                    message: '请输入您的密码'
+                  }
+                ]}>
+                <Input.Password autoComplete="off" prefix={<LockOutlined />} type="password" placeholder="密码" />
+              </Form.Item>
+              {/*手机令牌方式*/}
+              <Form.Item
+                name="code"
+                rules={[
+                  {
+                    min: 4,
+                    max: 4,
+                    message: '手机令牌验证码应该是 4 个数字'
+                  },
+                  {
+                    required: true,
+                    message: '请输入您的验证码'
+                  }
+                ]}>
+                <Input autoComplete="off" prefix={<InsuranceOutlined />} placeholder="手机令牌验证码" />
+              </Form.Item>
+              <Form.Item>
+                <Form.Item name="remember" valuePropName="checked" noStyle>
+                  <Checkbox>记住密码 |</Checkbox>
+                </Form.Item>
+                <span className="login-form-forgot">忘记密码？<a href="">找回密码</a></span>
+              </Form.Item>
+              <Form.Item style={{ margin: 0 }}>
+                <Button block type="primary" htmlType="submit">
+                  登录
+                </Button>
+              </Form.Item>
+              <Divider>或者使用其它方式直接登录</Divider>
+              <Button block color="primary" style={{ marginBottom: 15 }}>使用钉钉登录</Button>
+              <Button block color="danger">使用飞书登录</Button>
+            </Form>
+          </div>
+        </div>
         <div className="stellar-login-footer">
           <b>👻 STELLAR </b>© 2025 EZOPS.CN. <a href="" target='_blank'><GithubOutlined /> 立即查看项目开发进度.</a>
         </div>
