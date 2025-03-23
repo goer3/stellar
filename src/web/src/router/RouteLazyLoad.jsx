1c11ec6226bb04fa05e67777c298f754a31533cd
DK <1214966109@qq.com>
2025-03-23 21:05:23 +0800

    优化

--- src/web/src/router/RouteLazyLoad.jsx
+++ src/web/src/router/RouteLazyLoad.jsx
@@ -1,8 +1,18 @@
 import { Suspense } from 'react';
+import { Spin } from 'antd';
+
+// 路由加载动画
+const RouteLoading = () => {
+  return (
+    <div className="stellar-loading">
+      <Spin size="large" />
+    </div>
+  );
+};
 
 // 惰性加载，避免路由加载的时候所有页面都会被加载
 const RouteLazyLoad = (Component) => (
-  <Suspense fallback={<div>Loading...</div>}>
+  <Suspense fallback={<RouteLoading />}>
     <Component />
   </Suspense>
 );
